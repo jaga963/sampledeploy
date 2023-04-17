c1d24271f04a5cc979ea023a0555ab8f38f10516
jaga963 <105596259+jaga963@users.noreply.github.com>
2023-04-17 21:43:16 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash/
+echo "Download war filr from nexus"
+wget --user jaga --password jaga http://43.207.210.101:8081/nexus/service/local/repositories/jaga/content/my-app/my-app-1.0/28/my-app-1.0-28.jar
