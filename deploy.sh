6755f15ec2effdeff6ae86b87314e3d70add6430
jaga963 <105596259+jaga963@users.noreply.github.com>
2023-04-18 16:11:48 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash/
 echo "Download war filr from nexus"
-wget --user=jaga --password=jaga http://43.207.210.101:8081/nexus/service/local/repositories/jaga/content/hello-world/war/hello-world-war/45/hello-world-war-45.war
+wget --user=jaga --password=$1 http://43.207.210.101:8081/nexus/service/local/repositories/jaga/content/hello-world/war/hello-world-war/$2/hello-world-war-$2.war
